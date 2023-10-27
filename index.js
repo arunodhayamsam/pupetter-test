c26cb29a6c834feb9371154065986f5ba142e364
arunodhayamsam <100224916+arunodhayamsam@users.noreply.github.com>
2023-10-27 08:59:17 +0530

    Updated

---
+++ index.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    // Launch headless Chrome browser
+    const browser = await puppeteer.launch({
+        headless: true // Set to false if you want to see the browser UI
+    });
+
+    // Create a new page
+    const page = await browser.newPage();
+
+    try {
+        // Navigate to a website
+        await page.goto('https://google.com');
+
+        // Take a screenshot
+        await page.screenshot({ path: 'screenshot.png' });
+
+        // Get and print the page title
+        const pageTitle = await page.title();
+        console.log('Page Title:', pageTitle);
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        // Close the browser
+        await browser.close();
+    }
+})();
